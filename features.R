0ba9b0a7eee17b16de43df210e7ad3b5c25cf83d
samuelmcdermott <s.mcdermott@warwick.ac.uk>
2015-04-14 23:31:22 +0100

    more features

--- features.R
+++ features.R
@@ -1,11 +1,17 @@
 features <-function(input,k){
   require(RTextTools)
- #kfold stuff obvs
- input.matrix <- create_matrix(cbind(input$title,input$text), language = "english", removeNumbers = TRUE, stemWords = TRUE, weighting = weightTfIdf)
- input.corpus <-create_container(input.matrix, input$topic, trainSize = 1:20, testSize = 1:8, virgin = FALSE)
- svm <- cross_validate(input.corpus,9,algorithm = "SVM")
- #models <- train_models(input.corpus,algorithms = c("SVM","MAXENT"))
- #results <- classify_models(input.corpus, models)
- analytics <- create_analytics(input.corpus, svm)
- return(analytics)
+  input.matrix <- create_matrix(cbind(input$title,input$text), language = "english", removeNumbers = TRUE, stemWords = TRUE, weighting = weightTfIdf)
+#   lets fold this up
+#  for(fold in 1:k){
+#    #train using (k-1)n/k instances and test using n/k
+#    testSize <- floor((k-1)*n/k)
+#    testLower <-  ((fold-1)*testSize)+2
+#    testUpper<- testLower + testSize + 2
+#      }
+# input.corpus <-create_container(input.matrix,input$topic,  testSize = 1:floor(nrow(input)/10) , virgin = FALSE)
+# #trainSize = (1:testLower-1, testUpper+1:n), testSize =testLower:testUpper
+#  #models <- train_models(input.corpus,algorithms = c("SVM","MAXENT"))
+#  #results <- classify_models(input.corpus, models)
+#  analytics <- create_analytics(input.corpus, svm)
+ return(input.matrix)
 }

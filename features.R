ceeb79651bb6947df43ac5de1b9417b1a06db90a
samuelmcdermott <s.mcdermott@warwick.ac.uk>
2015-04-14 23:46:38 +0100

    got create_container to work :D

--- features.R
+++ features.R
@@ -1,15 +1,15 @@
 features <-function(input,k){
   require(RTextTools)
   input.matrix <- create_matrix(cbind(input$title,input$text), language = "english", removeNumbers = TRUE, stemWords = TRUE, weighting = weightTfIdf)
-#   lets fold this up
-#  for(fold in 1:k){
-#    #train using (k-1)n/k instances and test using n/k
-#    testSize <- floor((k-1)*n/k)
-#    testLower <-  ((fold-1)*testSize)+2
-#    testUpper<- testLower + testSize + 2
-#      }
-# input.corpus <-create_container(input.matrix,input$topic,  testSize = 1:floor(nrow(input)/10) , virgin = FALSE)
-# #trainSize = (1:testLower-1, testUpper+1:n), testSize =testLower:testUpper
+ # lets fold this up
+ for(fold in 1:k){
+   #train using (k-1)n/k instances and test using n/k
+   sizeOfTest <- floor((k-1)*nrow(input)/k)
+   testLower <-  ((fold-1)*sizeOfTest)+2
+   testUpper<- testLower + sizeOfTest + 2
+   input.corpus <-create_container(input.matrix,input$topic,trainSize = c(1:testLower-1, testUpper+1:nrow(input)), testSize =testLower:testUpper, virgin = FALSE)
+   }
+#
 #  #models <- train_models(input.corpus,algorithms = c("SVM","MAXENT"))
 #  #results <- classify_models(input.corpus, models)
 #  analytics <- create_analytics(input.corpus, svm)
